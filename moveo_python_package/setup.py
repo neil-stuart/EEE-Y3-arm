a54737ded5e78275873bb4f628ae046423cecd3b
Neil <neil.stuart11@gmail.com>
2024-03-08 15:33:32 +0000

    Update package description

--- moveo_python_package/setup.py
+++ moveo_python_package/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='g11_moveo',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'pyserial',
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Neil Stuart',
+    author_email='n.stuart3@universityofgalway.ie',
     description='A Python package to control the BCN3D Moveo robotic arm.',
     keywords='BCN3D Moveo, robotic arm, control'
 )
