dcd14eeb1f4135b8785ff9f02b4f4fd98c560086
Neil <neil.stuart11@gmail.com>
2024-03-08 12:22:26 +0000

    Test inverse kinematics

--- host_serial_control/ik.py
+++ host_serial_control/ik.py
@@ -2,14 +2,15 @@
 # If there is a matplotlib error, uncomment the next line, and comment the line below it.
 # %matplotlib inline
 # %matplotlib widget
+
 import matplotlib.pyplot as plt
 import ikpy.chain
 import numpy as np
 import ikpy.utils.plot as plot_utils
 
 my_chain = ikpy.chain.Chain.from_urdf_file("./host_serial_control/moveo.URDF")
-target_position = [ 0.15, -0.2, 0.4]
-print("The angles of each joints are : ", my_chain.inverse_kinematics(target_position))
+target_position = [ 0.0, -0.3, 0.5]
+print(my_chain.inverse_kinematics(target_position))
 
 
 fig, ax = plot_utils.init_3d_figure()
@@ -17,3 +18,5 @@ my_chain.plot(my_chain.inverse_kinematics(target_position), ax, target=target_po
 plt.xlim(-0.4, 0.4)
 plt.ylim(-0.4, 0.4)
 plt.show()
+
+# [0, BASE ROTATION*pi/2, FIRST JOINT * pi/2, SECOND JOINT]
