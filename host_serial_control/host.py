1850f2b714df69ebb5f0dda26bf91943a654ee7a
Neil <neil.stuart11@gmail.com>
2024-03-09 11:55:33 +0000

    Testing

--- host_serial_control/host.py
+++ host_serial_control/host.py
@@ -5,13 +5,20 @@ from g11_moveo import BCN3D_Moveo, Moveo_IK
 
 def test_motors():
     pi = math.pi
-    arm = BCN3D_Moveo('/dev/ttyUSB0')
+    #arm = BCN3D_Moveo('/dev/ttyUSB0')
+    ik = Moveo_IK()
+
     time.sleep(2)
 
-    arm.go_to(-pi/2,  -pi/2, pi/2,  -pi/2)
+    angles = ik.point_to_angles(0.3,0.3,0.10)
+
+    #arm.go_to(*angles)
+
+    ik.plot_manipulator_3d(*angles)
+    
     time.sleep(3)
 
-    arm.go_to(0,0,0,0)
+    #arm.go_to(0,0,0,0)
     time.sleep(3)
 
     # arm.go_to(-pi/2, pi/2, -pi/2, -pi/4)
@@ -20,7 +27,6 @@ def test_motors():
     # arm.go_to(0,0,0,0)
 
     print("Finished test.")
-    arm.nano.close()
 
 
 if __name__ == "__main__":
