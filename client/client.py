c4f00f15ce4389dd2da1932385785cb0b9f997e9
Neil Stuart <neil.stuart11@gmail.com>
2024-05-02 09:56:40 +0100

    improve code readability

--- client/client.py
+++ client/client.py
@@ -20,10 +20,8 @@ arm_pos = [0,0,0]
 points = [[0,0,0]]
 
 class Arm_Control(Frame):
-    def __init__(self, arm_move_function, parent, update_func=None, *args, **kwargs):
+    def __init__(self,  parent,  *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.arm_move_function = arm_move_function
-        self.update_func = update_func
         self.x_slider = Scale(self, from_=-0.4,resolution=0.005,sliderlength=50, length=300,to=0.4, orient="horizontal", label="X (m)")
         self.y_slider = Scale(self, from_=-0.4,resolution=0.005,sliderlength=50,length=300, to=0.4, orient="horizontal", label="Y (m)")
         self.z_slider = Scale(self, from_=0.55, resolution=0.005,sliderlength=50,length=300,to=-0.25, orient="vertical", label="Z (m)")
@@ -45,8 +43,8 @@ class Arm_Control(Frame):
         arm_pos[1] = self.y_slider.get()
         arm_pos[2] = self.z_slider.get()
         angles = ik.point_to_angles(*arm_pos)
-        self.update_func(*angles) 
-        self.arm_move_function(*angles)
+        ik.update_plot_ax(*angles) 
+        arm.go_to(*angles)
         ik_canvas.draw()
 
 
@@ -67,7 +65,7 @@ if __name__ == "__main__":
     container_frame.configure(bg='white')
 
     # Place the arm_control_widget in the container frame
-    arm_control_widget = Arm_Control(lambda theta0, theta1, theta2, theta3: arm.go_to(theta0,theta1,theta2,theta3), container_frame, update_func=ik.update_plot_ax)
+    arm_control_widget = Arm_Control(container_frame)
     arm_control_widget.grid(row=0, column=0)
     arm_control_widget.configure(bg='white')
 
